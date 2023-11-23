7c401efc7a64f1f251df41592b9d543f95055417
LinkWhale <filipbjed@gmail.com>
2023-11-23 11:32:48 +0100

    more comments

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,12 +6,12 @@ fetch("https://api.openweathermap.org/data/2.5/weather?lat=59.23705&lon=17.98192
 
 //function putting all the data needed into the approprieate elements 
 function test(data) {
-    document.getElementById("img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    document.getElementById("temp").innerHTML = Math.floor(data.main.temp - 273.15) + "&degC";
+    document.getElementById("img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`; //choose the current weather img id
+    document.getElementById("temp").innerHTML = Math.floor(data.main.temp - 273.15) + "&degC"; //convert Kelvin into Celsius
     document.getElementById("max_temp").innerHTML = `Max: ${Math.floor(data.main.temp_max - 273.15)}&degC`;
     document.getElementById("location").innerHTML = data.name;
     document.getElementById("min_temp").innerHTML = `Min: ${Math.floor(data.main.temp_min - 273.15)}&degC`;
     document.getElementById("humidity").innerHTML = `Humidity: ${data.main.humidity}`;
-    document.getElementById("wind").innerHTML = `Wind speed: ${Math.floor(data.wind.speed*60*60/1000)}km/h`;
+    document.getElementById("wind").innerHTML = `Wind speed: ${Math.floor(data.wind.speed*60*60/1000)}km/h`; //convert m/s to km/h
     console.log(data); //prints out the json file in console for ease of access
 }
