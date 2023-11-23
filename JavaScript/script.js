fbe9fd2160dcef0858b62107b60ed454f322cb86
LinkWhale <filipbjed@gmail.com>
2023-11-23 10:27:22 +0100

    comments

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,10 @@
+//fetch the api data from specified longitude and latitude with a unique API key
 fetch("https://api.openweathermap.org/data/2.5/weather?lat=59.23705&lon=17.98192&appid=cb99254c58061e7a36962034a20e9add")
 .then(API => API.json())
 .then(data => test(data))
 
+
+//function putting all the data needed into the approprieate elements 
 function test(data) {
     document.getElementById("img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     document.getElementById("temp").innerHTML = Math.floor(data.main.temp - 273.15) + "&degC";
@@ -10,5 +13,5 @@ function test(data) {
     document.getElementById("min_temp").innerHTML = `Min: ${Math.floor(data.main.temp_min - 273.15)}&degC`;
     document.getElementById("humidity").innerHTML = `Humidity: ${data.main.humidity}`;
     document.getElementById("wind").innerHTML = `Wind speed: ${Math.floor(data.wind.speed*60*60/1000)}km/h`;
-    console.log(data);
+    console.log(data); //prints out the json file in console for ease of access
 }
